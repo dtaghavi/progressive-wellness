56153994e4b61934fd07d9e8aacd7da0b7e89cbd
Daniel <daniel@siliconswamp.io>
2023-08-08 16:57:17 -0400

    section 1 and nav done

--- src/app/_components/slide-show/slideshow.component.ts
+++ src/app/_components/slide-show/slideshow.component.ts
@@ -6,13 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slideshow.component.scss']
 })
 export class SlideshowComponent implements OnInit {
-    images: string[] = [
-        '../../../assets/banner_1.png',
-        '../../../assets/banner_2.png',
-        '../../../assets/banner_3.png',
-        '../../../assets/banner_4.png',];
+  images: string[] = [
+    '../../../assets/banner_1.png',
+    '../../../assets/banner_2.png',
+    '../../../assets/banner_3.png',
+    '../../../assets/banner_4.png',];
 
-  constructor() {}
 
-  ngOnInit(): void {}
+    activeImage: number = 0;
+
+    get prev() {
+      return this.activeImage - 1 < 0 ? this.images.length - 1 : this.activeImage - 1;
+    }
+
+  constructor() {
+    setInterval(() => {
+      this.activeImage++;
+      if(this.activeImage >= this.images.length) this.activeImage = 0;
+    }, 5000);
+  }
+
+  ngOnInit(): void { }
 }
