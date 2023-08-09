0894a33393c1d7502309d6bb1da598ad439509f3
Daniel <detaghavi@gmail.com>
2023-08-09 00:26:12 -0400

    section one tweaks, section two done, section three started

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+
 
 @Component({
     selector: 'app-home',
@@ -6,9 +7,40 @@ import { Component } from "@angular/core";
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+    @ViewChild('animateDiv1') animateDiv1!: ElementRef;
+    @ViewChild('animateDiv2') animateDiv2!: ElementRef;
+
+    [key: string]: any;
     
+    isHidden1 = true;
+    isHidden2 = true;
 
     constructor() {
        
+    }
+
+    ngAfterViewInit() {
+        this.observeElement(this.animateDiv1, 'isHidden1');
+        this.observeElement(this.animateDiv2, 'isHidden2');
+    }
+
+    private observeElement(elementRef: ElementRef, property: string): void {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.1
+        };
+
+        const observer: IntersectionObserver = new IntersectionObserver((entries) => this.handleIntersect(entries, observer, property), options);
+        observer.observe(elementRef.nativeElement);
+    }
+
+    private handleIntersect(entries: IntersectionObserverEntry[], observer: IntersectionObserver, property: string): void {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                this[property] = false;
+                observer.unobserve(entry.target);
+            }
+        });
     }
 }
